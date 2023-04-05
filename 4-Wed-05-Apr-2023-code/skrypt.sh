8eefa3369ff279db883c829b43f6b84030669b1f
Piotr Krzysztof <piotrlis555@gmail.com>
2023-04-05 15:28:57 +0200

    script: added parametrized --logs option

--- 4-Wed-05-Apr-2023-code/skrypt.sh
+++ 4-Wed-05-Apr-2023-code/skrypt.sh
@@ -4,8 +4,16 @@ if [[ "$1" == '--date' ]]; then
   now=$(date +'%m/%d/%Y %R')
   echo "$now"
 elif [[ "$1" == '--logs' ]]; then
-  for i in {1..100}
+  n=100
+
+  if [ -n "$2" ]
+  then
+    n=$2
+  fi
+
+  for i in $(seq 1 $n)
   do
     touch log${i}.txt
   done
+
 fi
