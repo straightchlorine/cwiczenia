a7cd3679f4dc8b9695a5213f756cde6cd994a423
Piotr Krzysztof <piotrlis555@gmail.com>
2023-04-05 15:20:27 +0200

    script: creates 100 log files

--- 4-Wed-05-Apr-2023-code/skrypt.sh
+++ 4-Wed-05-Apr-2023-code/skrypt.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == '--date' ]]; then
   now=$(date +'%m/%d/%Y %R')
   echo "$now"
+elif [[ "$1" == '--logs' ]]; then
+  for i in {1..100}
+  do
+    touch log${i}.txt
+  done
 fi
